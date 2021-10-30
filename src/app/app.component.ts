e574e9b53c3076ac7dba0a65d4f3cd70c5606129
stefanoVe <stefano.vecchietti.99@gmail.com>
2021-10-30 21:58:38 +0200

    logo corretto e smooth scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgxSmoothScrollDirectiveOption } from '@eunsatio/ngx-smooth-scroll';
 import { NgbConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,6 +9,16 @@ import { NgbConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public options: NgxSmoothScrollDirectiveOption = {
+    duration: 500, // Global option
+    wheel: {
+      duration: 600, // This will override global option
+      timingFunction: 'ease-out',
+    },
+    keydown: {
+      timingFunction: 'ease-in',
+    },
+  };
   constructor(private router: Router, ngbConfig: NgbConfig) {
     ngbConfig.animation = true;
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ParallaxDirective } from './parallax.directive';
+import { NgxSmoothScrollModule } from '@eunsatio/ngx-smooth-scroll';
 
 @NgModule({
   declarations: [AppComponent, HomepageComponent, ParallaxDirective],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, NgxSmoothScrollModule],
   providers: [],
   bootstrap: [AppComponent],
 })
