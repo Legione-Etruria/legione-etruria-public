c6de1f22bd1d5642b6a78fb6275897c6955daa11
StefanoVe <stefano.vecchietti.99@gmail.com>
2022-02-06 20:24:41 +0100

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { NgxSmoothScrollDirectiveOption } from '@eunsatio/ngx-smooth-scroll';
 import { NgbConfig } from '@ng-bootstrap/ng-bootstrap';
@@ -9,6 +10,34 @@ import { NgbConfig } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  appRoutes = [
+    {
+      routerLink: '/home',
+      iconClass: 'fas fa-home',
+      label: 'Home',
+    },
+    {
+      routerLink: '/membri',
+      iconClass: 'fas fa-users',
+      label: 'Membri',
+    },
+    {
+      routerLink: '/contattaci',
+      iconClass: 'fas fa-info',
+      label: 'Gioca con noi',
+    },
+    {
+      routerLink: '/chi-siamo',
+      iconClass: 'fas fa-question',
+      label: 'Chi Siamo',
+    },
+    {
+      routerLink: '/members/my-legio',
+      iconClass: 'fas fa-street-view',
+      label: '',
+    },
+  ];
+
   public options: NgxSmoothScrollDirectiveOption = {
     duration: 500, // Global option
     wheel: {
@@ -19,11 +48,12 @@ export class AppComponent {
       timingFunction: 'ease-in',
     },
   };
-  constructor(private router: Router, ngbConfig: NgbConfig) {
+  constructor(
+    private router: Router,
+    ngbConfig: NgbConfig,
+    private titleService: Title
+  ) {
     ngbConfig.animation = true;
-  }
-
-  checkRoute() {
-    return this.router.url;
+    titleService.setTitle('Legione Etruria');
   }
 }
