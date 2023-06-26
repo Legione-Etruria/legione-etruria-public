4c81f72401f036a0b15ddd791cb8f0106a042955
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-06-26 11:15:37 +0200

    aggiunge pan

--- src/app/containers/homepage/homepage.component.ts
+++ src/app/containers/homepage/homepage.component.ts
@@ -16,4 +16,27 @@ export class HomepageComponent implements OnInit {
     console.log('visitLink', link);
     window.open(link);
   }
+
+  rotateItem(event: MouseEvent, coeff = 50) {
+    const card = event.currentTarget as HTMLElement;
+    const rect = card.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const cardWidth = card.offsetWidth;
+    const cardHeight = card.offsetHeight;
+
+    const centerX = cardWidth / 2;
+    const centerY = cardHeight / 2;
+
+    const rotateX = (centerY - y) / coeff;
+    const rotateY = (x - centerX) / coeff;
+    const translateZ = Math.abs((centerY - y) / 10);
+
+    card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(${translateZ}px)`;
+  }
+
+  resetItem(event: MouseEvent) {
+    const card = event.currentTarget as HTMLElement;
+    card.style.transform = '';
+  }
 }
