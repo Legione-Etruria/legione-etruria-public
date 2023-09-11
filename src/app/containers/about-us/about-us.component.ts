e60b0eebb9e318fba2a4b87c071802c1d7e3cc2e
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-09-11 23:51:07 +0200

    vari fix e modifiche

--- src/app/containers/about-us/about-us.component.ts
+++ src/app/containers/about-us/about-us.component.ts
@@ -13,25 +13,4 @@ export class AboutUsComponent {
   constructor(titleService: Title, public utilsService: UtilsService) {
     titleService.setTitle('Chi siamo');
   }
-
-  public images = [
-    'assets/images/members/about-us-cover-photo.jpeg',
-    'assets/images/members/akira.jpeg',
-    'assets/images/members/ariete.jpeg',
-    'assets/images/members/combo1.jpeg',
-    'assets/images/members/combo2.jpeg',
-    'assets/images/members/combo3.jpeg',
-    'assets/images/members/drill.jpeg',
-    'assets/images/members/germanico.jpeg',
-    'assets/images/members/johnny.jpeg',
-    'assets/images/members/main-wallpaper.jpg',
-    'assets/images/members/mosca.jpeg',
-    'assets/images/members/price.jpeg',
-    'assets/images/members/rerum.jpeg',
-    'assets/images/members/scorpion.jpeg',
-    'assets/images/members/smith.jpeg',
-    'assets/images/members/spike.jpeg',
-    'assets/images/members/taurus.jpeg',
-    'assets/images/members/vecho.jpeg',
-  ];
 }
