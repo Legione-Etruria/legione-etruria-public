d4dd0767919982f050ae69e3b0e4e6d1fce3ee98
StefanoVe <stefano.vecchietti.99@gmail.com>
2025-03-30 23:58:59 +0200

    nav, migliora componenti

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -31,4 +31,36 @@ export class UtilsService {
   public scrollToAnchor(anchor: string): void {
     this._scroller.scrollToAnchor(anchor);
   }
+
+  public currentTailwindMediaQuery():
+    | 'none'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | '2xl' {
+    const width = window?.innerWidth;
+
+    if (width >= 1536) {
+      return '2xl';
+    }
+
+    if (width >= 1280) {
+      return 'xl';
+    }
+
+    if (width >= 1024) {
+      return 'lg';
+    }
+
+    if (width >= 768) {
+      return 'md';
+    }
+
+    if (width >= 640) {
+      return 'sm';
+    }
+
+    return 'none';
+  }
 }
